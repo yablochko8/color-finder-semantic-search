aa6a28fd6d4b165bb77f7830fbd50b909682707f
Local Dev <local@localhost>
2025-06-25 16:20:18 +0100

    openai integration

---
+++ src/clientOpenai.ts
@@ -0,0 +1,13 @@
+import OpenAI, { ClientOptions } from "openai";
+
+const apiKey = process.env.OPENAI_API_KEY;
+
+if (!apiKey) {
+  throw new Error("OPENAI_API_KEY is not set");
+}
+
+const opts: ClientOptions = {
+  apiKey,
+};
+
+export const clientOpenai = new OpenAI(opts);
